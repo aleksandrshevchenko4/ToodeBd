a1bb220f705c9b4d1499ef65ece9ba868d99b654
aleksandrshevchenko4 <62281789+aleksandrshevchenko4@users.noreply.github.com>
2020-12-10 10:52:30 +0200

    Add files via upload

---
+++ dbo.Tootetable.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Tootetable] (
+    [Id]           INT          IDENTITY (1, 1) NOT NULL,
+    [ToodeNimetus] VARCHAR (50) NULL,
+    [Kogus]        INT          NULL,
+    [Hind]         REAL         NULL,
+	[Pilt] VARCHAR(50) NULL,
+	[Kategooria_Id] INT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+	FOREIGN KEY([Kategooria_Id]) REFERENCES [dbo].[Kategooria_Id] ([Id])
+);
+
